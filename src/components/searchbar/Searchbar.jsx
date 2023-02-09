3d98c74b782d297d10b8a135cf4cbf05dea58989
Diana064 <dipustolyakova02@gmail.com>
2023-02-09 18:35:05 +0200

    add use effect searchbar

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './Searchbar.module.css';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
-export class Searchbar extends Component {
-  state = {
-    pictureName: '',
-  };
 
-  handleNameChange = event => {
-    this.setState({ pictureName: event.currentTarget.value.toLowerCase() });
+export default function Searchbar({ onSubmit }) {
+  const [pictureName, setPictureName] = useState('');
+  const handleNameChange = event => {
+    setPictureName(event.currentTarget.value.toLowerCase());
   };
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.pictureName.trim() === '') {
+    if (pictureName.trim() === '') {
       toast.error('🖼️ Введіть будь ласка назву картинки!');
       return;
     }
-    this.props.onSubmit(this.state.pictureName);
-    this.reset();
+    onSubmit(pictureName);
+    reset();
   };
-  reset = () => {
-    this.setState({ pictureName: '' });
+  const reset = () => {
+    setPictureName('');
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchForm_button}>
-            <SearchIcon width="30px" />
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchForm_button}>
+          <SearchIcon width="30px" />
+        </button>
 
-          <input
-            className={css.SearchForm_input}
-            type="text"
-            placeholder="Search images and photos"
-            value={this.state.pictureName}
-            onChange={this.handleNameChange}
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={css.SearchForm_input}
+          type="text"
+          placeholder="Search images and photos"
+          value={pictureName}
+          onChange={handleNameChange}
+        />
+      </form>
+    </header>
+  );
 }
